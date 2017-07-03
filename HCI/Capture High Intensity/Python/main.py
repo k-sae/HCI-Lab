008457584407d9d45bc26ee99bef85729e4ac966
Billy <billy.fcih@yahoo.com>
2017-07-03 19:40:57 +0200

    Add a file chooser to upload an image in capture highest intensity

--- HCI/Capture High Intensity/Python/main.py
+++ HCI/Capture High Intensity/Python/main.py
@@ -1,8 +1,14 @@
 from HighestIntensity import *
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
-#From an image
-highestIntensity = HighestIntensity('a.jpg')
-highestIntensity.showImage()
+Tk().withdraw()
+filename = askopenfilename(filetypes=(("Image files", "*.jpg"), ("Image files", "*.png")))
+
+if filename != '':
+    #From an image
+    highestIntensity = HighestIntensity(filename)
+    highestIntensity.showImage()
 
 #From a camera
 #highestIntensity.setStream(1)
