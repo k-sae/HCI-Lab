733fa2307fb10cd4f391fc5427ffd09613deaf01
خالد هشام <khaledhesham2017@hotmail.com>
2017-07-03 22:24:38 +0200

    delete  print  statment in python grayscale

--- HCI/Image to Gray/python/task1.py
+++ HCI/Image to Gray/python/task1.py
@@ -3,9 +3,9 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 Tk().withdraw()
 filepath = askopenfilename(filetypes=(("Image files", "*.jpg"), ("Image files", "*.png")))
+
 if filepath != '':
     filename = (filepath.split('/')[-1]).split('.')[0]
-    print (filename)
     image =cv2.imread(filepath)
     gray_image =cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     cv2.imshow(filename,image)
