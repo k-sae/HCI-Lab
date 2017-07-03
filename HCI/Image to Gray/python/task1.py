4cb0f302c62591cb5425de8fa426e32e69da8f20
Billy <billy.fcih@yahoo.com>
2017-07-03 19:49:53 +0200

    Add a file chooser to upload an image in 'Image to gray'

--- HCI/Image to Gray/python/task1.py
+++ HCI/Image to Gray/python/task1.py
@@ -1,7 +1,14 @@
 import cv2
-image =cv2.imread('landscap.jpg')
-gray_image =cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-cv2.imshow('lanscap',image)
-cv2.imshow('landscap_gray',gray_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+Tk().withdraw()
+filename = askopenfilename(filetypes=(("Image files", "*.jpg"), ("Image files", "*.png")))
+
+if filename != '':
+    image =cv2.imread(filename)
+    gray_image =cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    cv2.imshow(filename,image)
+    cv2.imshow(filename+'_gray',gray_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
