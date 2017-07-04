92e49659d92bb5dac33595f4ba0ac138a560b30f
mostafa7azem <mostafahazem144@yahoo.com>
2017-07-04 14:05:46 +0200

    saving a safepoint

--- HCI/get owned/getOwned/Source.cpp
+++ HCI/get owned/getOwned/Source.cpp
@@ -5,11 +5,91 @@
 #include "guiclass.h"
 using namespace cv;
 using namespace std;
+
+class theif{
+
+};
+
+
+Mat image;
+string winName = "desplay window";
+Rect button, button2;
+int level=0;
+void Startgame() {
+	Mat frame;
+	double minVal;
+	double maxVal;
+	Point minLoc;
+	Point maxLoc;
+	Mat gray;
+	VideoCapture cap(0);
+	if (!cap.isOpened())
+	{
+		cout << "Cannot open the web cam" << endl;
+	}
+	while (true)
+	{
+		bool bSuccess = cap.read(frame);
+		if (!bSuccess)
+		{
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
+		cvtColor(frame, gray, CV_BGR2GRAY);
+		minMaxLoc(gray, &minVal, &maxVal, &minLoc, &maxLoc);
+		circle(frame, maxLoc, 5, (255, 0, 0), 2);
+		imshow("displaywindow", frame);
+		if (waitKey(30) == 27)
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+	}
+	waitKey(0);
+}
+void callBackFunc(int event, int x, int y, int flags, void* userdata)
+{
+	if ( y > 500 && y < 550)
+	{
+		if (event == EVENT_LBUTTONDOWN)
+		{
+			if (button.contains(Point(x, y)))
+			{
+				cout << "Clicked!" << endl;
+				rectangle(image, button, Scalar(0, 0, 255), 2);
+				Startgame();
+			}
+		}
+		if (event == EVENT_LBUTTONUP)
+		{
+			rectangle(image, button, Scalar(200, 200, 200), 2);
+		}
+
+		imshow(winName, image);
+		waitKey(1);
+	}if (y > 600 && y < 650) {
+		if (event == EVENT_LBUTTONDOWN)
+		{
+			if (button2.contains(Point(x, y)))
+			{
+				cout << "Clicked!" << endl;
+				rectangle(image, button2, Scalar(0, 0, 255), 2);
+			}
+		}
+		if (event == EVENT_LBUTTONUP)
+		{
+			rectangle(image, button2, Scalar(200, 200, 200), 2);
+		}
+
+		imshow(winName, image);
+		waitKey(1);
+	}
+}
+	
 int main() 
 {
 	
-	Mat image = imread("newback.png", CV_LOAD_IMAGE_COLOR);
-	Rect button,button2;
+ image = imread("newback.png", CV_LOAD_IMAGE_COLOR);
 	string buttonText("play as cop");
 	string buttonText2("play as theif");
     button= Rect(0,500,image.cols, 50);
@@ -18,10 +98,11 @@ int main()
 	image(button2) = Vec3b(200, 200, 200);
 	putText(image, buttonText, Point(button.width*0.40, 500+button.height*0.5), FONT_HERSHEY_PLAIN, 1, Scalar(0, 0, 0));
 	putText(image, buttonText2, Point(button.width*0.40,600 + button.height*0.5), FONT_HERSHEY_PLAIN, 1, Scalar(0, 0, 0));
-	namedWindow("Display window", CV_WINDOW_AUTOSIZE);
-	imshow("Display window", image);
+	namedWindow(winName, CV_WINDOW_AUTOSIZE);
+	namedWindow(winName);
+	setMouseCallback(winName, callBackFunc);
+	imshow(winName, image);
     waitKey();
-
     return 0;
 }
 // TODO 
