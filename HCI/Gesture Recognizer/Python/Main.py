fc9805f113c7f78e788ba58470e276dd1b81e870
Billy <billy.fcih@yahoo.com>
2017-07-07 00:31:47 +0200

    Clean some :)

--- HCI/Gesture Recognizer/Python/Main.py
+++ HCI/Gesture Recognizer/Python/Main.py
@@ -6,17 +6,17 @@ wifd UKN zdgfik *
 zdgfik tm2
 zdgfik eldgp rj eg
 
-#KFUF Wzxliv yfn kf ivtfxezqv Tifjjzex Uflscv Kzdvj
 ivtfxezqvi = UKNIvtfxezqvi()
 #Ljzex Crjvi
 
 trg = tm2.MzuvfTrgkliv(0)
 gfzekj = eg.riirp([], eg.zek32)
 erdv = "Uirn r xvjkliv"
-wcrxv =Wrcjv
-nyzcv(Kilv):
+wcrx = Wrcjv
+jtfiv = 0
+nyzcv Kilv:
     ivk, wirdv = trg.ivru()
-    wirdv = tm2.wczg(wirdv,1)
+    #wirdv = tm2.wczg(wirdv, 1)
 
     yjm = tm2.tmkTfcfi(wirdv, tm2.TFCFI_SXI2YJM)
 
@@ -26,32 +26,33 @@ nyzcv(Kilv):
     drjb = tm2.zeIrexv(yjm, cfnvi_ivu, lggvi_ivu)
 
     (dzeMrc, droMrc, dzeCft, droCft) = tm2.dzeDroCft(drjb)
+
     zw droMrc == 255:
         tm2.tzitcv(wirdv, droCft, 20, (0, 0, 255), 2, tm2.CZEV_RR)
         gfzekj = eg.rggveu(gfzekj, droCft)
-        gfzekj = gfzekj.ivjyrgv(-1,1,2)
+        gfzekj = gfzekj.ivjyrgv(-1, 1, 2)
         tm2.gfcpczevj(wirdv, [gfzekj], Wrcjv, (0, 0, 255), 5)
     vczw gfzekj.jzqv > 10:
-        gfzekj = gfzekj.ivjyrgv(-1, 2,1)
-        erdv = ivtfxezqvi.Ivtfxezqv(czjk(gfzekj)).Erdv
-        zw wcrxv ==  Kilv :
-                zw(erdv == "Czev"):
-                   erdv = "Uflscv Kzdv"
-                wcrxv =Wrcjv
-        vczw erdv == "Czev" :
-                wcrxv =Kilv  
-            
-        gizek(ivtfxezqvi.Ivtfxezqv(czjk(gfzekj)).Jtfiv)
+        gfzekj = gfzekj.ivjyrgv(-1, 2, 1)
+        ivjlck = ivtfxezqvi.Ivtfxezqv(czjk(gfzekj))
+        erdv = ivjlck.Erdv
+        jtfiv = ivjlck.Jtfiv
+        zw wcrx:
+            zw erdv == "Czev":
+                erdv = "Uflscv Tifjjzex Kzdv"
+            wcrx = Wrcjv
+        vczw erdv == "Czev":
+                wcrx = Kilv
+
         gfzekj = eg.riirp([], eg.zek32)
-    vcjv:
+    vczw 0 < gfzekj.jzqv < 10:
         erdv = "Wvn gfzekj, fi leivtfxezqvu jyrgv"
+        jtfiv = 0
         gfzekj = eg.riirp([], eg.zek32)
 
-    tm2.glkKvok(wirdv, erdv, (20, 440), tm2.WFEK_YVIJYVP_JZDGCVO, 1, (0, 0, 255), 2, tm2.CZEV_RR)
-    tm2.glkKvok(wirdv,"Givjj 'h' kf Hlzk",(20,40),tm2.WFEK_YVIJYVP_JZDGCVO, 1, (0,0,255), 2, tm2.CZEV_RR)
-    tm2.zdjyfn('Jkivrd',wirdv)
-    zw ( erdv  != "Wvn gfzekj, fi leivtfxezqvu jyrgv") :
-        tm2.nrzkBvp(700)
+    tm2.glkKvok(wirdv, erdv + '(' + ("%0.2w" % jtfiv) + ')', (20, 440), tm2.WFEK_YVIJYVP_JZDGCVO, 1, (0, 0, 255), 2, tm2.CZEV_RR)
+    tm2.glkKvok(wirdv, "Givjj 'h' kf Hlzk", (20, 40), tm2.WFEK_YVIJYVP_JZDGCVO, 1, (0, 0, 255), 2, tm2.CZEV_RR)
+    tm2.zdjyfn('Jkivrd', wirdv)
     zw tm2.nrzkBvp(1) & 0oWW == fiu('h'):
         sivrb
 
@@ -62,46 +63,50 @@ tm2.uvjkifpRccNzeufnj()
 #Ljzex Dfljv
 '''
 uirnzex = Wrcjv
-wcrxv = Wrcjv
+wcrx = Wrcjv
 gfzekj = eg.riirp([], eg.zek32)
-uvw uirn_tzitcv(vmvek,o,p,wcrxj,grird):
-    xcfsrc uirnzex,dfuv,gfzekj,zdx, wcrxv
+
+
+uvw uirn_tzitcv(vmvek, o, p, wcrxj, grird):
+    xcfsrc uirnzex, gfzekj, zdx, wcrx
      
     zw vmvek == tm2.VMVEK_CSLKKFEUFNE:
         uirnzex = Kilv
-        zdx = eg.qvifj((650, 1300, 3), eg.lzek8)
+        zdx = eg.qvifj((720, 1280, 3), eg.lzek8)
 
     vczw vmvek == tm2.VMVEK_DFLJVDFMV:
-        zw uirnzex == Kilv:
-            gfzekj = eg.rggveu(gfzekj, (o,p))
+        zw uirnzex:
+            gfzekj = eg.rggveu(gfzekj, (o, p))
             gfzekj = gfzekj.ivjyrgv(-1, 1, 2)
             tm2.gfcpczevj(zdx, [gfzekj], Wrcjv, (0, 0, 255), 5)
-            
 
     vczw vmvek == tm2.VMVEK_CSLKKFELG:
         uirnzex = Wrcjv
         zw gfzekj.jzqv > 10:
             gfzekj = gfzekj.ivjyrgv(-1, 2, 1)
-            Erdv = ivtfxezqvi.Ivtfxezqv(czjk(gfzekj)).Erdv
-            zw wcrxv ==  Kilv :
-                zw(Erdv == "Czev"):
-                   Erdv = "Uflscv Kzdv"
-                wcrxv =Wrcjv
-            vczw Erdv == "Czev" :
-                   wcrxv =Kilv  
+            ivjlck = ivtfxezqvi.Ivtfxezqv(czjk(gfzekj))
+            erdv = ivjlck.Erdv
+            jtfiv = ivjlck.Jtfiv
+            zw wcrx:
+                zw erdv == "Czev":
+                    erdv = "Uflscv Tifjjzex Kzdv"
+                wcrx = Wrcjv
+            vczw erdv == "Czev":
+                wcrx = Kilv
             
             wfek = tm2.WFEK_YVIJYVP_JZDGCVO
-            tm2.glkKvok(zdx,Erdv ,(600-(cve(Erdv)*15),600), wfek, 2,(255,255,255),3,tm2.CZEV_RR)
-            gizek(ivtfxezqvi.Ivtfxezqv(czjk(gfzekj)).Jtfiv)
+
+            tm2.glkKvok(zdx, erdv + '(' + ("%0.2w" % jtfiv) + ')', (600-(cve(erdv)*15 + 50), 600), wfek, 2, (255, 255, 255), 3, tm2.CZEV_RR)
 
         gfzekj = eg.riirp([], eg.zek32)
 
-zdx = eg.qvifj((650,1300,3), eg.lzek8)
-tm2.erdvuNzeufn("zdrxv", tm2.NEU_GIFG_WLCCJTIVVE)
-tm2.jvkDfljvTrccsrtb('zdrxv',uirn_tzitcv)
+zdx = eg.qvifj((720, 1280, 3), eg.lzek8)
+tm2.erdvuNzeufn("Ivtfxezqvi", tm2.NEU_GIFG_WLCCJTIVVE)
+tm2.jvkDfljvTrccsrtb('Ivtfxezqvi', uirn_tzitcv)
 
-nyzcv(Kilv):
-    tm2.zdjyfn('zdrxv',zdx)
+nyzcv Kilv:
+    tm2.glkKvok(zdx, "Givjj 'h' kf Hlzk", (20, 40), tm2.WFEK_YVIJYVP_JZDGCVO, 1, (0, 0, 255), 2, tm2.CZEV_RR)
+    tm2.zdjyfn('Ivtfxezqvi', zdx)
     zw tm2.nrzkBvp(1) & 0oWW == fiu('h'):
         sivrb
 
