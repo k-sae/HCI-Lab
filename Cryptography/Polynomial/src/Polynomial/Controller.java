3ee99346c4ff92c89359eafc1179afd4c9e4a206
kareem <karim.salah2048@gmail.com>
2017-07-24 18:50:45 +0200

    add basic project files for Polynomial

---
+++ Cryptography/Polynomial/src/Polynomial/Controller.java
@@ -0,0 +1,4 @@
+package Polynomial;
+
+public class Controller {
+}
---
+++ Cryptography/Polynomial/src/Polynomial/Polynomial.java
@@ -0,0 +1,31 @@
+package Polynomial;
+
+import java.math.BigInteger;
+
+/**
+ * Created by kemo on 24/07/2017.
+ */
+public class Polynomial {
+    private BigInteger[] coefficients;
+
+    public Polynomial(BigInteger[] coefficients){
+        this.coefficients = coefficients;
+    }
+
+    //TODO
+    public static Polynomial rand()
+    {
+        return null;
+    }
+    //TODO
+    public BigInteger evaluate(BigInteger x){
+
+        return null;
+    }
+
+    //TODO
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
