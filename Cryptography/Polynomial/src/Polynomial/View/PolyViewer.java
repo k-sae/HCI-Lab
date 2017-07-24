16cf46233507d5cfb1ab4b17e85d99b6a8145ab9
kareem <karim.salah2048@gmail.com>
2017-07-24 22:05:52 +0200

    add eq viewer

--- Cryptography/Polynomial/src/Polynomial/View/PolyViewer.java
+++ Cryptography/Polynomial/src/Polynomial/View/PolyViewer.java
@@ -1,10 +1,17 @@
 package Polynomial.View;
 
 import Polynomial.Polynomial;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import org.scilab.forge.jlatexmath.TeXConstants;
+import org.scilab.forge.jlatexmath.TeXFormula;
+
+import java.awt.image.BufferedImage;
 
 /**
  * Created by kemo on 24/07/2017.
@@ -12,6 +19,7 @@ import javafx.scene.paint.Color;
 public class PolyViewer extends HBox {
     private Polynomial polynomial;
     private Label basicPolynomialViewer;
+    private ImageView equation;
     public PolyViewer()
     {
         basicPolynomialViewer = new Label();
@@ -21,7 +29,8 @@ public class PolyViewer extends HBox {
         //1-add textBox for x
         //2-add Evaluate button ( dont add listeners i will add them)
         //3-do some tweaks (padding width whatever
-        vBox.getChildren().addAll(basicPolynomialViewer);
+        equation = new ImageView();
+        vBox.getChildren().addAll(basicPolynomialViewer,equation );
         getChildren().addAll(vBox);
     }
     public void setPolynomial(Polynomial polynomial)
@@ -32,6 +41,10 @@ public class PolyViewer extends HBox {
     private void updateUi()
     {
         basicPolynomialViewer.setText(polynomial.toString());
+        TeXFormula tex = new TeXFormula(polynomial.toString());
+        java.awt.Image awtImage = tex.createBufferedImage(TeXConstants.STYLE_TEXT, 12, java.awt.Color.BLACK, null);
+        Image fxImage = SwingFXUtils.toFXImage((BufferedImage) awtImage, null);
+         equation.setImage(fxImage);
     }
 
 }
