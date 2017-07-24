d95a4903152bb7fc03f1422cda14735236f3eb18
Amr-Sameh <amrsameh.develop@gmail.com>
2017-07-24 23:36:13 +0200

    start impliment Polynomial View

--- Cryptography/Polynomial/src/Polynomial/Main.java
+++ Cryptography/Polynomial/src/Polynomial/Main.java
@@ -4,10 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
