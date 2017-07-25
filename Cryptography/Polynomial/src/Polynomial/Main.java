84c3e3893111d61f8b26ed69f25fa222946ac1e5
kareem <karim.salah2048@gmail.com>
2017-07-25 15:24:27 +0200

    add polynomial interpolator library

--- Cryptography/Polynomial/src/Polynomial/Main.java
+++ Cryptography/Polynomial/src/Polynomial/Main.java
@@ -9,6 +9,14 @@ import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.apache.commons.math.MathRuntimeException;
+import org.apache.commons.math.analysis.interpolation.SplineInterpolator;
+import org.apache.commons.math.analysis.polynomials.PolynomialFunctionLagrangeForm;
+import org.apache.commons.math.analysis.polynomials.PolynomialSplineFunction;
+import org.apache.commons.math.analysis.solvers.NewtonSolver;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Main extends Application {
     @Override
@@ -19,6 +27,14 @@ public class Main extends Application {
         Parent root = new MainWindow();
         primaryStage.setTitle("Polynomial Evaluator");
         primaryStage.setScene(new Scene(root, 1280,720 ));
+        double x[] = { -1.0, 0.0, 1.0,2.0 };
+        double y[] = { 1.0, 1.0, 3.0,1.0};
+        PolynomialFunctionLagrangeForm polynomialFunctionLagrangeForm  = new PolynomialFunctionLagrangeForm(x,y);
+        System.out.println("coefficients:");
+        for (int i = polynomialFunctionLagrangeForm.getCoefficients().length - 1; i >=0 ; i--) {
+            System.out.println(polynomialFunctionLagrangeForm.getCoefficients()[i]);
+        }
+
         primaryStage.show();
     }
 
