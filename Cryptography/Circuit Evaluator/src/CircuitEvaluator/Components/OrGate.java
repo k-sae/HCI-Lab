c67592ebb8412c75303f848524386db554fc2082
kareem <karim.salah2048@gmail.com>
2017-07-29 02:48:55 +0200

    add constructors for and and or gate

--- Cryptography/Circuit Evaluator/src/CircuitEvaluator/Components/OrGate.java
+++ Cryptography/Circuit Evaluator/src/CircuitEvaluator/Components/OrGate.java
@@ -4,6 +4,12 @@ package CircuitEvaluator.Components;
  * Created by kareem on 7/29/17.
  */
 public abstract class OrGate<T> extends Gate<T> {
+    public OrGate(Wire<T> input1, Wire<T> input2, Wire<T> output)
+    {
+        getInputWires().add(input1);
+        getInputWires().add(input2);
+        setOutput(output);
+    }
     @Override
     public T computeOutput() {
         return null;
