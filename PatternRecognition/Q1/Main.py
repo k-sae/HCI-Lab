1c1838ad003d35e3bb63375cf77eb7f1bbb46efe
Billy <billy.fcih@yahoo.com>
2017-07-31 19:49:33 +0200

    Finish part 1 from question 1, Pattern recognition

--- PatternRecognition/Q1/Main.py
+++ PatternRecognition/Q1/Main.py
@@ -1,8 +1,12 @@
 import scipy.stats as stats
 
-a, b = 50, 10
-mu, sigma = 37, 2
-dist = stats.truncnorm((a - mu) / sigma, (b - mu) / sigma, loc=mu, scale=sigma)
+a, b = 10, 50
+mean, stdDev, limit = 37, 2, 42
+
+z = (limit - mean) / stdDev
+probability = stats.norm.sf(abs(z))
+
+dist = stats.truncnorm((a - mean) / stdDev, (b - mean) / stdDev, loc=mean, scale=stdDev)
 
 count = [0]
 values = [0]
@@ -10,7 +14,7 @@ for num in range(10):
     values[num] = dist.rvs(10000)
     # noinspection PyTypeChecker
     for val in values[num]:
-        if val > 42:
+        if val > limit:
             count[num] += 1
     print(count[num])
     count.append(0)
