0224ca10a8e0b80be3d54373a1da5d4e4bbe4cd8
amr-sameh <amrsameh.develop@gmail.com>
2017-08-01 18:18:22 +0200

    fix q4 plot

--- PatternRecognition/Q4/main.py
+++ PatternRecognition/Q4/main.py
@@ -2,9 +2,7 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 start = 150
-fathersTall=np.arange(150.0, 190.0, 0.4)
-
-
+fathersTall=np.arange(150.0, 190.00, 0.4)
 sonsTalls = []
 for father in fathersTall:
     start=0
@@ -12,13 +10,18 @@ for father in fathersTall:
     while start<100:
         temp.append(father*0.7+random.uniform(-1,1))
         start+=1
-
     sonsTalls.append(temp)
-
 for son in sonsTalls:
     print (son)
-
-plt.scatter(sonsTalls,bins=30, normed=True, alpha=0.6)
+drawx=[]
+drawy = []
+for father , son in zip(fathersTall , sonsTalls):
+    for tall in son :
+        drawx.append(father)
+        drawy.append(tall)
+plt.xticks(np.arange(150,190,0.4))
+plt.scatter(drawx,drawy)
 plt.xlabel("Heights of fathers")
 plt.ylabel("Heights of sons")
 plt.show()
+
