e04118f591e0f4f03be030354db4710aeb1d4c1e
Billy <billy.fcih@yahoo.com>
2017-08-03 21:10:55 +0200

    Generate a random dataset

--- PatternRecognition/HW2/Q1/main.py
+++ PatternRecognition/HW2/Q1/main.py
@@ -2,23 +2,26 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
-iris = datasets.load_digits(n_class=10)
+#Generate datasets
+X, y = datasets.make_classification(n_samples=50, n_classes=2, n_features=4)
 
-X = iris.data
+iris = datasets.load_iris()
+#X = iris.data
+#y = iris.target
 print(X)
-y = iris.target
 print(y)
 print(len(X[0]))
 print(len(X))
-target_names = iris.target_names
-print(target_names)
+print(len(y))
 
-lda = LinearDiscriminantAnalysis(n_components=2)
+lda = LinearDiscriminantAnalysis()
+
 X_r = lda.fit(X, y).transform(X)
+print(X_r[0])
 
 colors = ['red', 'blue', 'green','black', 'yellow', 'gray']
 plt.figure()
-for color, i, target_name in zip(colors, [0, 1, 2, 3,4,5,6], target_names):
+for color, i in zip(colors, [0, 1]):
     plt.scatter(X_r[y == i, 0], X_r[y == i, 1], alpha=.8, color=color)
 
 plt.title('LDA')
