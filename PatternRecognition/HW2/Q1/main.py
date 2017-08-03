1337dc0ffbb2ebfb122ada94d5538798e646e04b
kareem <karim.salah2048@gmail.com>
2017-08-03 22:47:40 +0200

    add basic code for q1 2 and 3

--- PatternRecognition/HW2/Q1/main.py
+++ PatternRecognition/HW2/Q1/main.py
@@ -2,27 +2,9 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
-#Generate datasets
-X, y = datasets.make_classification(n_samples=150, n_classes=3, n_features=4, shuffle=False, n_redundant=0, n_informative=3)
+#Generate datasets of 10 samples
+X, y = datasets.make_classification(n_samples=10, n_classes=2, n_features=4, shuffle=False, n_redundant=0, n_informative=3)
 
-iris = datasets.load_iris()
-#X = iris.data
-#y = iris.target
 print(X)
+
 print(y)
-print(len(X[0]))
-print(len(X))
-print(len(y))
-
-lda = LinearDiscriminantAnalysis()
-
-X_r = lda.fit(X, y).transform(X)
-print(X_r[0])
-
-colors = ['red', 'blue', 'green','black', 'yellow', 'gray']
-plt.figure()
-for color, i in zip(colors, [0, 1]):
-    plt.scatter(X_r[y == i, 0], X_r[y == i, 1], alpha=.8, color=color)
-
-plt.title('LDA')
-plt.show()
---
+++ PatternRecognition/HW2/Q2/main.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+#Generate datasets
+X, y = datasets.make_classification(n_samples=10, n_classes=3, n_features=4, shuffle=False, n_redundant=0, n_informative=3)
+
+print(X)
+print(y)
+print(len(X[0]))
+print(len(X))
+print(len(y))
+
+lda = LinearDiscriminantAnalysis()
+
+
+
+X_r = lda.fit(X, y).transform(X)
+print(X_r[0])
+
+colors = ['red', 'blue', 'green','black', 'yellow', 'gray']
+plt.figure()
+for color, i in zip(colors, [0, 1]):
+    plt.scatter(X_r[y == i, 0], X_r[y == i, 1], alpha=.8, color=color)
+
+plt.title('LDA')
+plt.show()
