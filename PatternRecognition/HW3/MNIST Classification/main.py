817a03a61468b254654bc0091a1c9f922173b804
Billy <billy.fcih@yahoo.com>
2017-08-04 23:27:25 +0200

    Add datasets

--- PatternRecognition/HW3/MNIST Classification/main.py
+++ PatternRecognition/HW3/MNIST Classification/main.py
@@ -1,14 +1,13 @@
 import pandas as pd
 from mnist import MNIST
+import os
 
-mndata = MNIST('datasets')
+path = os.path.join('datasets', 'mnist')
+
+mndata = MNIST(path)
 
 #Using MNIST, with the original files
 images, labels = mndata.load_training()
 images2, labels2 = mndata.load_testing()
 
-#Using Pandas, with CSV
-df = pd.read_csv("datasets\mnist_train.csv")
-
-print(df.shape)
 print(len(images[0]))
